0f113675ecc8a5fbf1ee0116c86ca51142e57e66
sandeepforever007 <snalla@uncc.edu>
2017-09-30 14:33:36 -0500

    initial commit

---
+++ WebAPIJQeury/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebAPIJQeury.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ WebAPIJQeury/Controllers/EmployeeServiceController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIJQeury.Controllers
+{
+    public class EmployeeServiceController : ApiController
+    {
+        // GET: api/EmployeeService
+        public IEnumerable<Employee> Get()
+        {
+			EmployeeDBEntities db = new EmployeeDBEntities();
+
+			return db.Employees.ToList();
+        }
+
+        // GET: api/EmployeeService/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/EmployeeService
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/EmployeeService/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/EmployeeService/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
